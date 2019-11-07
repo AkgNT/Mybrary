3850181303a4fe864d17d6e404f5782a6009f4c7
Nils T <info@akademigarden.se>
2019-11-07 14:15:08 +0100

    Author routes

---
+++ routes/authors.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//  ALL AUTHORS
+router.get('/', async (req, res) => {
+
+    let searchOptions = {}
+
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+
+
+
+
+})
+
+//  NEW AUTHOR
+router.get('/new', (req, res) => {
+    res.render('authors/new', {
+        author: new Author()
+    })
+})
+
+//  CREATE AUTHOR 
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render('authors/new', {
+    //             author: author,
+    //             errorMessage: 'Error creating author'
+    //         })
+    //         console.log(err)
+    //     } else {
+    //         //res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    // })
+
+
+})
+
+module.exports = router
